f0b73b16fac5f73157ef7332353268083ce98f3e
Abdulloh <abdullajonmamnazirov@gmail.com>
2023-06-05 20:34:29 +0500

    add: doughnut and bar charts

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { Bar,Doughnut } from "react-chartjs-2";
+import { Chart } from "chart.js/auto";
 import Card from "../Componenets/Card";
 import Navbar from "../Componenets/Navbar";
 
@@ -14,6 +16,39 @@ const Home = () => {
     getData();
   }, []);
 
+  let doughnut = {
+    data: {
+      labels: ["Teachers", "Marketing", "Cleaners", "Administrators", "Others"],
+      datasets: [
+        {
+          data: [150, 60, 70, 80, 90],
+          backgroundColor: ["red", "green", "blue", "purple", "yellow"],
+        },
+      ],
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+  };
+  
+  const labels = ["January", "February", "March", "April", "May"];
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: "Total students",
+        backgroundColor: "rgb(50, 153, 249)",
+        borderColor: "rgb(255, 99, 132)",
+        data: [170, 200, 185, 240, 280, 300],
+      },
+      {
+        label: "Graduators",
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgb(255, 99, 132)",
+        data: [25, 20, 12, 25, 40, 48],
+      },
+    ],
+  };
+
   return (
     <>
       <Navbar title="Dashboard" />
@@ -22,6 +57,20 @@ const Home = () => {
         <Card title="Total Teachers:" num={stats?.[2]?.total} />
         <Card title="Total Graduators:" num={stats?.[1]?.total} />
         <Card title="Total Courses:" num={stats?.[3]?.total} />
+      </div>
+      <div className="my-10">
+        <h2 className="text-center">Staff Statistics</h2>
+        <div className="w-2/5 mx-auto">
+          <Doughnut
+            responsive={"true"}
+            maintainaspectratio={"true"}
+            data={doughnut.data}
+          />
+        </div>
+        <div className="mt-10">
+          <h2 className="text-center">Annual Statistics</h2>
+          <Bar data={data} />
+        </div>
       </div>
     </>
   );
